169494b8dad33a9fa5c3aecff64c0e065944b128
Aditya <avaswani@ucsb.edu>
2026-01-10 12:13:45 -0800

    Requirements + Cmake

---
+++ build/CMakeFiles/vibe_core.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vibe_core.dir/vibeCheckCore.cpp.o"
+  "CMakeFiles/vibe_core.dir/vibeCheckCore.cpp.o.d"
+  "vibe_core.cpython-313-darwin.so"
+  "vibe_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vibe_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
